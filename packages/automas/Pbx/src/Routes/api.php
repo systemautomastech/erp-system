56ec4e5725516be6fedc65dd6e5b545b839f500e
mesbah-meju <uddin.mesbaah@gmail.com>
2026-08-20 18:20:18 +0600

    first commit

--- packages/automas/Pbx/src/Routes/api.php
+++ packages/automas/Pbx/src/Routes/api.php
@@ -6,17 +6,16 @@ use Automas\Pbx\Http\Controllers\ClickToCallController;
 use Automas\Pbx\Http\Controllers\CallerLookupController;
 
 Route::middleware([
-    'web',
-    'auth',
+    'auth:sanctum',
     'verified',
     'PlanModuleCheck:Pbx',
 ])->group(function () {
-        Route::get('/pbx/webrtc-config', [WebRtcConfigController::class, 'index'])
-            ->name('pbx.webrtc-config');
+    Route::get('/pbx/webrtc-config', [WebRtcConfigController::class, 'index'])
+        ->name('pbx.webrtc-config');
 
-        Route::post('/pbx/click-to-call', [ClickToCallController::class, 'call'])
-            ->name('pbx.click-to-call');
+    Route::post('/pbx/click-to-call', [ClickToCallController::class, 'call'])
+        ->name('pbx.click-to-call');
 
-        Route::get('/pbx/caller-lookup', [CallerLookupController::class, 'lookup'])
-            ->name('pbx.caller-lookup');
-    });
+    Route::get('/pbx/caller-lookup', [CallerLookupController::class, 'lookup'])
+        ->name('pbx.caller-lookup');
+});
