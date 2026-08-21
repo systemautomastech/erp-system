e1fb251f37bcfb8f4ae5050292644de64ca69bae
unknown <uddin.mesbaah@gmail.com>
2026-08-21 18:32:19 +0600

    adding api of pbx  directory

--- packages/automas/Pbx/src/Routes/api.php
+++ packages/automas/Pbx/src/Routes/api.php
@@ -11,12 +11,15 @@ Route::middleware([
     'verified',
     'PlanModuleCheck:Pbx',
 ])->group(function () {
-        Route::get('/pbx/webrtc-config', [WebRtcConfigController::class, 'index'])
-            ->name('pbx.webrtc-config');
+    Route::get('/pbx/webrtc-config', [WebRtcConfigController::class, 'index'])
+        ->name('pbx.webrtc-config');
 
-        Route::post('/pbx/click-to-call', [ClickToCallController::class, 'call'])
-            ->name('pbx.click-to-call');
+    Route::post('/pbx/click-to-call', [ClickToCallController::class, 'call'])
+        ->name('pbx.click-to-call');
 
-        Route::get('/pbx/caller-lookup', [CallerLookupController::class, 'lookup'])
-            ->name('pbx.caller-lookup');
-    });
+    Route::get('/pbx/caller-lookup', [CallerLookupController::class, 'lookup'])
+        ->name('pbx.caller-lookup');
+
+    Route::get('/pbx/extensions/directory', [PbxExtensionController::class, 'directory'])
+        ->name('pbx.directory');
+});
