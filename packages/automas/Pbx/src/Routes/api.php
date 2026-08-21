59ac9129419c3f5f7c838e5c8a54ad8afa40767b
unknown <uddin.mesbaah@gmail.com>
2026-08-21 21:49:58 +0600

    redisign and reform

--- packages/automas/Pbx/src/Routes/api.php
+++ packages/automas/Pbx/src/Routes/api.php
@@ -4,9 +4,10 @@ use Illuminate\Support\Facades\Route;
 use Automas\Pbx\Http\Controllers\WebRtcConfigController;
 use Automas\Pbx\Http\Controllers\ClickToCallController;
 use Automas\Pbx\Http\Controllers\CallerLookupController;
+use Automas\Pbx\Http\Controllers\PbxExtensionController;
 
 Route::middleware([
-    'auth:sanctum',
+    'auth',
     'verified',
     'PlanModuleCheck:Pbx',
 ])->group(function () {
@@ -18,7 +19,22 @@ Route::middleware([
 
     Route::get('/pbx/caller-lookup', [CallerLookupController::class, 'lookup'])
         ->name('pbx.caller-lookup');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    'verified',
+    'PlanModuleCheck:Pbx',
+])->prefix('app')->group(function () {
+    Route::get('/pbx/webrtc-config', [WebRtcConfigController::class, 'index'])
+        ->name('app.pbx.webrtc-config');
+
+    Route::post('/pbx/click-to-call', [ClickToCallController::class, 'call'])
+        ->name('app.pbx.click-to-call');
+
+    Route::get('/pbx/caller-lookup', [CallerLookupController::class, 'lookup'])
+        ->name('app.pbx.caller-lookup');
 
     Route::get('/pbx/extensions/directory', [PbxExtensionController::class, 'directory'])
-        ->name('pbx.directory');
+        ->name('app.pbx.directory');
 });
